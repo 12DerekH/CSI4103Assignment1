cbc5ad3695a97025fae4e99a066aeb934e72a2da
12DerekH <12hacaultd@gmail.com>
2023-01-23 17:59:49 -0500

    Done with web app
    Just testing left

--- interest_ing/src/App.js
+++ interest_ing/src/App.js
@@ -6,53 +6,163 @@ const Header = (props) => {
     <h1 className='header'>{props.name}</h1>
   )
 }
-/*
-const ListOptions = (props) => {
-  const allOptions = [];
 
-  for(let i = 0; i < props.length; i++) {
-    allOptions.push(<option>{props.name}</option>)
-  }
+//API and code to call API supplied by https://apilayer.com/marketplace/exchangerates_data-api
+//in compliance with their terms of service for personal and academic use
 
-  return(
-    {allOptions}
-  )
-
-}*/
-
-// Based on the example of using react forms from https://www.w3schools.com/react/react_forms.asp
 const Part = (props) => {
-  const [selectedOption, setSelectedOption] = useState('');
-  const allOptions = [];
+  const [outputOption, setOutputOption] = useState('CAD');
+  const [inputAmount, setInputAmount] = useState(5);
+  const [exchangeRate, setExchangeRate] = useState(1);
+  const [allRates, setAllRates] = useState([]);
 
-  console.log("In Part: ", props);
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "HrOISLRc8t6NFvy8OeMQY4bdG187ki54");
+
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+
+  const allOptions = [];
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(selectedOption);
+    console.log('Output: ', outputOption);
+
+    console.log("calling API")
+
+    fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=USD%2CEUR%2CGBP&base=CAD", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result.rates)
+      setAllRates(result.rates)
+    }
+    )
+    .catch(error => console.log('error', error));
+
+    console.log('Exchange rate: ', allRates);
+
+    if({outputOption} === "USD"){
+      setExchangeRate(allRates[1])
+      console.log('Exchange rate: ', exchangeRate)
+    } else {
+      console.log('failed')
+    }
+
   };
 
   for(let i = 0; i < props.currencies.length; i++) {
     allOptions.push(<option key={i}>{props.currencies[i].name}</option>)
   }
-  console.log("After loop: ", allOptions);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <select value={selectedOption} onChange={event => setSelectedOption(event.target.value)}>
-        <option> </option>
-        {allOptions}
-      </select>
-      <button type="submit">Submit</button>
-    </form>
+    <div className='Currency-container'>
+
+      <form onSubmit={handleSubmit} className='item'>
+        <p className='item'>Load exchange rates: 
+  
+        </p>   
+        <p> 
+          <button type="submit">Submit</button>      
+        </p>
+      </form>
+      <label className='large'>CAD       1 :  {allRates.USD} USD</label>
+      <label className='large'>CAD       1 :  {allRates.GBP} EUR</label>
+      <label className='large'>CAD       1 :  {allRates.EUR} EUR</label>
+    </div>
   );
 }
+
+
+// Code used to create drop down menu is based on code provided by chat.openai.com (quiry: "How can I create a drop down menu in React?")
+/*
+const Part = (props) => {
+  const [inputOption, setInputOption] = useState('CAD');
+  const [outputOption, setOutputOption] = useState('CAD');
+  const [inputAmount, setInputAmount] = useState(5);
+  const [exchangeRate, setExchangeRate] = useState(1);
+  const [allRates, setAllRates] = useState([]);
+
+ 
+
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "HrOISLRc8t6NFvy8OeMQY4bdG187ki54");
+
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+
+  const allOptions = [];
+
+
+  //allRates = [outputAmount[0],outputAmount[1],outputAmount[2]];  
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Input: ', inputOption);
+    console.log('Output: ', outputOption);
+
+    console.log("calling API")
+
+    fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=USD%2CEUR%2CGBP&base=CAD", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result.rates)
+      setAllRates(result.rates)
+    }
+    )
+    .catch(error => console.log('error', error));
+
+    console.log('Exchange rate: ', allRates);
+  };
+
+  for(let i = 0; i < props.currencies.length; i++) {
+    allOptions.push(<option key={i}>{props.currencies[i].name}</option>)
+  }
+
+  return (
+    <div className='Currency-container'>
+
+      <form onSubmit={handleSubmit} className='item'>
+        <select value={inputOption} onChange={event => setInputOption(event.target.value)}>
+          <option>CAD</option>  
+        </select>
+        <label>{props.name}:    
+        <input 
+        type="text"
+        value={inputAmount}
+        onChange={(e) => setInputAmount(e.target.value)}
+        />
+      </label>
+      <button type="submit">Submit</button>
+      </form>
+
+      <p className='item'>Load exchange rates:</p>
+
+      <form onSubmit={handleSubmit} className='item'>
+        <select value={outputOption} onChange={event => setOutputOption(event.target.value)}>
+          <option> </option>
+          {allOptions}
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+
+    </div>
+  );
+}*/
 
 const Content = (props) => {
   console.log("In Content: ", props.parts);
   return (
     <div className='App-text'>
-      <p>Amount</p>
+      <div className='container'>
+        <p className='item'></p>
+        <p className='item'>Conversion</p>        
+      </div>
       <Part currencies={props.parts} />
     </div>
   )
@@ -64,19 +174,12 @@ const App = () => {
     currency: [
       {
         name: 'USD',
-        fetched: false 
       },
       {
         name: 'EUR',
-        fetched: false 
       },
       {
         name: 'GBP',
-        fetched: false 
-      },
-      {
-        name: 'CAD',
-        fetched: false 
       },
     ]
   }
