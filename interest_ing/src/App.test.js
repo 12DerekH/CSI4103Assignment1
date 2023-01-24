a20d594a3019b0d409573b652df9b9f0a1bc48d6
12DerekH <12hacaultd@gmail.com>
2023-01-24 10:24:42 -0500

    Basic test set up
    And a bit of other clean up

--- interest_ing/src/App.test.js
+++ interest_ing/src/App.test.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import {rest} from 'msw'
-import {setupServer} from 'msw/node'
 import {render, fireEvent, waitFor, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
-import Fetch from '../fetch'
-
+import App from './App.js'
+/*
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
+
+//All testing code taken from https://testing-library.com/docs/react-testing-library/example-intro
+
+test('loads and displays greeting', async () => {
+ 
+
+  // Arrange
+  render(<App />);
+  // Act
+  fireEvent.click(screen.getByText('Press to load exchange rates'))
+  // Assert
+})
